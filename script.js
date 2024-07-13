7e8c1946c52c6fad7e4a1bd05b40920c5b543a0d
NOBODYHERE2024 <majdjaffelinew2023@gmail.com>
2024-07-13 10:01:25 +0100

    Very soon ...

---
+++ script.js
@@ -0,0 +1,11 @@
+//This is my first JavaScript try!!!!
+//console.log('Hello')
+//alert('Hello everyoner')
+let myAge=12;
+let humanDogRatio=3;
+let myDogAge=myAge*humanDogRatio;
+console.log(myDogAge);
+let pieceOfBread=0.2$
+let breadPieces=4
+let priceOfBread=pieceOfBread*breadPieces
+console.log(priceOfBread)
